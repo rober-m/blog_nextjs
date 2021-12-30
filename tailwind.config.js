ca5bb916cd3379d5442dcb5299b46ed5c28ea98b
Robertino <48034748+llPatternll@users.noreply.github.com>
2021-12-30 11:48:59 -0300

    Added some base styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
 		'./components/**/*.{js,ts,jsx,tsx}',
 	],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				ubuntu: ['Ubuntu', 'sans-serif'],
+				ubuntuMono: ['Ubuntu Mono', 'monospace'],
+			},
+		},
 	},
 	plugins: [],
 };
