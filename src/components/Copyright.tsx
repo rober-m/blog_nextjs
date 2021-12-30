5187e7d032c233d9b505330c36939f4c387a5580
Robertino <48034748+llPatternll@users.noreply.github.com>
2021-12-30 12:19:05 -0300

    Did some styling and changed "about" text.

--- src/components/Copyright.tsx
+++ src/components/Copyright.tsx
@@ -1,15 +1,15 @@
 export default function Copyright() {
-  return (
-    <>
-      <p>&copy; 2020</p>
-      <style jsx>
-        {`
-          p {
-            font-size: 0.75rem;
-            text-align: center;
-          }
-        `}
-      </style>
-    </>
-  );
+	return (
+		<>
+			<p>&copy; 2020-{new Date().getFullYear()}</p>
+			<style jsx>
+				{`
+					p {
+						font-size: 0.75rem;
+						text-align: center;
+					}
+				`}
+			</style>
+		</>
+	);
 }
--- src/components/Date.tsx
+++ src/components/Date.tsx
@@ -1,19 +1,19 @@
-import { format, formatISO } from "date-fns";
+import { format, formatISO } from 'date-fns';
 
 type Props = {
-  date: Date;
+	date: Date;
 };
 export default function Date({ date }: Props) {
-  return (
-    <time dateTime={formatISO(date)}>
-      <span>{format(date, "LLLL d, yyyy")}</span>
-      <style jsx>
-        {`
-          span {
-            color: #9b9b9b;
-          }
-        `}
-      </style>
-    </time>
-  );
+	return (
+		<time className='text-gray-500' dateTime={formatISO(date)}>
+			<span>Last update: {format(date, 'LLLL d, yyyy')}</span>
+			<style jsx>
+				{`
+					span {
+						color: #9b9b9b;
+					}
+				`}
+			</style>
+		</time>
+	);
 }
