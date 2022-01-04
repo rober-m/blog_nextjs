bac02fb2ad413659139664ac1e3ff496026d866a
Robertino <48034748+llPatternll@users.noreply.github.com>
2022-01-04 18:22:18 -0300

    reinstalled tailwindcss

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,26 @@
-import Layout from "../components/Layout";
-import BasicMeta from "../components/meta/BasicMeta";
-import OpenGraphMeta from "../components/meta/OpenGraphMeta";
-import TwitterCardMeta from "../components/meta/TwitterCardMeta";
-import { SocialList } from "../components/SocialList";
+import Layout from '../components/Layout';
+import BasicMeta from '../components/meta/BasicMeta';
+import OpenGraphMeta from '../components/meta/OpenGraphMeta';
+import TwitterCardMeta from '../components/meta/TwitterCardMeta';
+import { SocialList } from '../components/SocialList';
 
 export default function Index() {
-  return (
-    <Layout>
-      <BasicMeta url={"/"} />
-      <OpenGraphMeta url={"/"} />
-      <TwitterCardMeta url={"/"} />
-      <div className="container">
-        <div>
-          <h1>
-            Hi, We're Next.js & Netlify<span className="fancy">.</span>
-          </h1>
-          <span className="handle">@nextjs-netlify-blog</span>
-          <h2>A blog template with Next.js and Netlify.</h2>
-          <SocialList />
-        </div>
-      </div>
-      <style jsx>{`
+	return (
+		<Layout>
+			<BasicMeta url={'/'} />
+			<OpenGraphMeta url={'/'} />
+			<TwitterCardMeta url={'/'} />
+			<div className='container'>
+				<div className=' bg-red-500 text-3xl'>
+					<h1>
+						Hi, We're Next.js & Netlify<span className='fancy'>.</span>
+					</h1>
+					<span className='handle'>@nextjs-netlify-blog</span>
+					<h2>A blog template with Next.js and Netlify.</h2>
+					<SocialList />
+				</div>
+			</div>
+			{/* <style jsx>{`
         .container {
           display: flex;
           align-items: center;
@@ -56,7 +56,7 @@ export default function Index() {
             font-size: 2.25rem;
           }
         }
-      `}</style>
-    </Layout>
-  );
+      `}</style> */}
+		</Layout>
+	);
 }
