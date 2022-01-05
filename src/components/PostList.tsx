58748aa5dbc8cd0388bc22a81859652728397052
Robertino <48034748+llPatternll@users.noreply.github.com>
2022-01-04 23:04:40 -0300

    styled PostList page

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,84 +1,55 @@
-import React from "react";
-import { PostContent } from "../lib/posts";
-import PostItem from "./PostItem";
-import TagLink from "./TagLink";
-import Pagination from "./Pagination";
-import { TagContent } from "../lib/tags";
+import React from 'react';
+import { PostContent } from '../lib/posts';
+import PostItem from './PostItem';
+import TagLink from './TagLink';
+import Pagination from './Pagination';
+import { TagContent } from '../lib/tags';
 
 type Props = {
-  posts: PostContent[];
-  tags: TagContent[];
-  pagination: {
-    current: number;
-    pages: number;
-  };
+	posts: PostContent[];
+	tags: TagContent[];
+	pagination: {
+		current: number;
+		pages: number;
+	};
 };
 export default function PostList({ posts, tags, pagination }: Props) {
-  return (
-    <div className={"container"}>
-      <div className={"posts"}>
-        <ul className={"post-list"}>
-          {posts.map((it, i) => (
-            <li key={i}>
-              <PostItem post={it} />
-            </li>
-          ))}
-        </ul>
-        <Pagination
-          current={pagination.current}
-          pages={pagination.pages}
-          link={{
-            href: (page) => (page === 1 ? "/posts" : "/posts/page/[page]"),
-            as: (page) => (page === 1 ? null : "/posts/page/" + page),
-          }}
-        />
-      </div>
-      <ul className={"categories"}>
-        {tags.map((it, i) => (
-          <li key={i}>
-            <TagLink tag={it} />
-          </li>
-        ))}
-      </ul>
-      <style jsx>{`
-        .container {
-          display: flex;
-          margin: 0 auto;
-          max-width: 1200px;
-          width: 100%;
-          padding: 0 1.5rem;
-        }
-        ul {
-          margin: 0;
-          padding: 0;
-        }
-        li {
-          list-style: none;
-        }
-        .posts {
-          display: flex;
-          flex-direction: column;
-          flex: 1 1 auto;
-        }
-        .posts li {
-          margin-bottom: 1.5rem;
-        }
-        .post-list {
-          flex: 1 0 auto;
-        }
-        .categories {
-          display: none;
-        }
-        .categories li {
-          margin-bottom: 0.75em;
-        }
-
-        @media (min-width: 769px) {
-          .categories {
-            display: block;
-          }
-        }
-      `}</style>
-    </div>
-  );
+	return (
+		<div className={' max-w-5xl flex mx-auto w-full px-6'}>
+			<div className='flex flex-col w-full '>
+				<ul className={'block lg:hidden my-4'}>
+					{tags.map((it, i) => (
+						<li key={i} className=' inline-block mx-2'>
+							<TagLink tag={it} />
+						</li>
+					))}
+				</ul>
+				<hr className=' block lg:hidden  text-gray-300 mb-4' />
+				<div className={'w-full flex flex-col'}>
+					<ul className={''}>
+						{posts.map((it, i) => (
+							<li key={i} className='mb-6'>
+								<PostItem post={it} />
+							</li>
+						))}
+					</ul>
+					<Pagination
+						current={pagination.current}
+						pages={pagination.pages}
+						link={{
+							href: (page) => (page === 1 ? '/posts' : '/posts/page/[page]'),
+							as: (page) => (page === 1 ? null : '/posts/page/' + page),
+						}}
+					/>
+				</div>
+			</div>
+			<ul className={' ml-6 hidden lg:block'}>
+				{tags.map((it, i) => (
+					<li key={i}>
+						<TagLink tag={it} />
+					</li>
+				))}
+			</ul>
+		</div>
+	);
 }
