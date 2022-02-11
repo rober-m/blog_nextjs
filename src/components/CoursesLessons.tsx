ea184eceac532c6dfcaf8cb330b21a2db8ef5214
Robertino <48034748+llPatternll@users.noreply.github.com>
2022-02-11 20:09:44 -0300

    added short course abstract to CoursesLessons

--- src/components/CoursesLessons.tsx
+++ src/components/CoursesLessons.tsx
@@ -8,12 +8,39 @@ type Props = {
 	tag: TagContent;
 };
 
+const coursesAbstract: { [x: string]: any }[] = [
+	{
+		'Haskell Course': (
+			<p>
+				This is a free course for complete beginners provided by{' '}
+				<a className='underline' href='https://twitter.com/robertinom_en'>
+					Robertino Martinez
+				</a>
+				.
+			</p>
+		),
+	},
+	{
+		'Other Course': (
+			<p>
+				This course is a free course for complete beginners provided by{' '}
+				<a href='https://twitter.com/robertinom_en'>Robertino Martinez</a>
+			</p>
+		),
+	},
+];
+
 export default function TagPostList({ posts, tag }: Props) {
 	return (
 		<div className={'container w-full max-w-5xl mx-auto px-6 flex flex-col'}>
-			<h1 className=' text-4xl lg:text-5xl capitalize mb-8 text-center text-violet-700'>
-				<span>{tag.name} Lessons</span>
+			<h1 className='text-4xl lg:text-5xl capitalize text-center text-violet-700'>
+				{tag.name} Lessons
 			</h1>
+			{coursesAbstract.filter((x) => x[tag.name])[0][tag.name] && (
+				<div className='mb-8 text-center text-sm text-gray-500'>
+					{coursesAbstract.filter((x) => x[tag.name])[0][tag.name]}
+				</div>
+			)}
 			<ul>
 				{posts.map((it, i) => (
 					<li key={i}>
