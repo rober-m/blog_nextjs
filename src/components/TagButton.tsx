6a508f9f3c72b055f88b3607bfa5674773f924a2
Robertino <48034748+llPatternll@users.noreply.github.com>
2022-02-15 15:33:23 -0300

    Modified TagButton style and colors in general

--- src/components/TagButton.tsx
+++ src/components/TagButton.tsx
@@ -8,21 +8,13 @@ export default function TagButton({ tag }: Props) {
 	return (
 		<>
 			<Link href={'/posts/tags/[[...slug]]'} as={`/posts/tags/${tag.slug}`}>
-				<a>{tag.name}</a>
+				<a className='inline-block rounded mt-2 py-1 px-2 text-violet-600 hover:text-violet-600 active:text-violet-600 bg-violet-200 hover:bg-violet-300 active:bg-violet-300'>
+					{tag.name}
+				</a>
 			</Link>
 			<style jsx>{`
 				a {
-					display: inline-block;
-					border-radius: 3px;
-					background-color: rgba(21, 132, 125, 0.2);
-					color: #15847d;
 					transition: background-color 0.3s ease;
-					padding: 0.25em 0.5em;
-					margin-top: 0.5rem;
-				}
-				a:active,
-				a:hover {
-					background-color: rgba(21, 132, 125, 0.4);
 				}
 			`}</style>
 		</>
