f4af3d40aa389e2c1a7061ec0afdd39fb58719a8
Robertino <48034748+llPatternll@users.noreply.github.com>
2022-02-17 13:52:44 -0300

    Solved tailwindCSS not working in mdx content

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
 		'./src/pages/**/*.{js,ts,jsx,tsx}',
 		'./src/components/**/*.{js,ts,jsx,tsx}',
 		'./src/lib/**/*.{js,ts,jsx,tsx}',
+		'./content/**/*.{md,mdx}'
 	],
 	theme: {
 		extend: {
