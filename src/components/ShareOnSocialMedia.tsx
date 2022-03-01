c02538798cb3c10ea161938e1ef0fce500df638d
Robertino <48034748+llPatternll@users.noreply.github.com>
2022-03-01 19:56:36 -0300

    changed share it url

--- src/components/ShareOnSocialMedia.tsx
+++ src/components/ShareOnSocialMedia.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 
-//TODO: Doesn't work because I don't have window when generating posts.
-//TODO: See if I can use just the slug with domain hardcoded or if I can use window generating posts.
 export const ShareOnSocialMedia = () => {
 	return (
 		<a
-			href={`https://twitter.com/share?url=${window.location.href}`}
+			href={'https://twitter.com/share?url=https://robertinom.com/posts/haskell-course-lesson-0'}
 			target='_blank'
 		>
-			share it
+			share it on Twitter
 		</a>
 	);
 };
