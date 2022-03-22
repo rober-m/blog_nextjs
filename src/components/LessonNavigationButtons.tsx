15a83880978b218c37dab26d97be23dfc1a53d58
Robertino <48034748+llPatternll@users.noreply.github.com>
2022-03-22 17:03:12 -0300

    changed LessonNavigationButtons style

--- src/components/LessonNavigationButtons.tsx
+++ src/components/LessonNavigationButtons.tsx
@@ -7,11 +7,11 @@ interface IProps {
 
 export const LessonNavigationButtons: React.FC<IProps> = ({ nextLesson }) => {
 	return (
-		<div className='flex flex-row justify-end mt-6'>
+		<div className='flex flex-row justify-between mt-6'>
 			<Link href='/courses/haskell-course'>
 				<a
 					style={{ textDecoration: 'none' }}
-					className='p-2 mx-2 border border-gray-400 rounded-md shadow hover:bg-violet-100 hover:scale-103 transition-transform active:shadow-none active:scale-100'
+					className='p-2 mx-2 border border-gray-400 rounded-md shadow hover:bg-violet-100 hover:scale-103 hover:shadow-md transition-transform active:shadow-none active:scale-100'
 				>
 					Course Page
 				</a>
@@ -20,7 +20,7 @@ export const LessonNavigationButtons: React.FC<IProps> = ({ nextLesson }) => {
 				<Link href={`/posts/${nextLesson}`}>
 					<a
 						style={{ textDecoration: 'none' }}
-						className='p-2 mx-2 border border-gray-400 rounded-md shadow hover:bg-violet-100 hover:scale-103 transition-transform active:shadow-none active:scale-100'
+						className='p-2 mx-2 border border-gray-400 rounded-md shadow hover:bg-violet-100 hover:scale-103 hover:shadow-md transition-transform active:shadow-none active:scale-100'
 					>
 						Next Lesson
 					</a>
