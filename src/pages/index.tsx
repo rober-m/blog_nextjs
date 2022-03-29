fcd430557a59a714bd0a40b8f447efa0c4763aca
Robertino <48034748+llPatternll@users.noreply.github.com>
2022-03-29 15:46:02 -0300

    troubleshooting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,23 +5,23 @@ import TwitterCardMeta from '../components/meta/TwitterCardMeta';
 import { SocialList } from '../components/SocialList';
 
 export default function Index() {
-	return (
-		<Layout>
-			<BasicMeta url={'/'} />
-			<OpenGraphMeta url={'/'} />
-			<TwitterCardMeta url={'/'} />
-			<div className='container flex justify-center items-center px-6'>
-				<div className='max-w-md'>
-					<h1 className='md:text-5xl'>Hi, I'm Robertino Martinez</h1>
-					<span className='inline-block mt-1 text-gray-500 font-ubuntuMono'>
-						@robertinom_en
-					</span>
-					<h2 className='before:hidden md:text-4xl'>
-						Engineer, technical founder, Blockchain developer
-					</h2>
-					<SocialList />
-				</div>
-			</div>
-		</Layout>
-	);
+  return (
+    <Layout>
+      <BasicMeta url={'/'} />
+      <OpenGraphMeta url={'/'} />
+      <TwitterCardMeta />
+      <div className='container flex justify-center items-center px-6'>
+        <div className='max-w-md'>
+          <h1 className='md:text-5xl'>Hi, I'm Robertino Martinez</h1>
+          <span className='inline-block mt-1 text-gray-500 font-ubuntuMono'>
+            @robertinom_en
+          </span>
+          <h2 className='before:hidden md:text-4xl'>
+            Engineer, technical founder, Blockchain developer
+          </h2>
+          <SocialList />
+        </div>
+      </div>
+    </Layout>
+  );
 }
